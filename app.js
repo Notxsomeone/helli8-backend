326433d2fc97e36c2630370f5f0777f6720da446
unknown <imnotxsomeone@gmail.com>
2023-12-12 13:56:21 +0330

    MongoDB integration

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('connected to mongodb'))
+    .catch(err => console.error('Could not connect to mongodb server', err));
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  creatingDate: { type: Date, default: Date.now }
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+async function createUser() {
+const testUser = UserModel({
+  username: 'mostafa',
+});
+
+const result = await testUser.save();
+console.log(result);
+}
+
+createUser();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

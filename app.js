90497abcc1e7ab9dbc2f24cdb34270165b7b5ddd
unknown <imnotxsomeone@gmail.com>
2023-12-19 14:25:38 +0330

    Added sign in

--- app.js
+++ app.js
@@ -6,30 +6,37 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('connected to mongodb'))
-    .catch(err => console.error('Could not connect to mongodb server', err));
+mongoose.connect('mongodb://localhost/users')
+    .then(() => console.log('connected to mongodb: users collection'))
+    .catch(err => console.error('Could not connect to mongodb server: users collection', err));
 
 const userSchema = new mongoose.Schema({
   username: String,
+  nickname: String,
+  
   creatingDate: { type: Date, default: Date.now }
 });
 
 const UserModel = mongoose.model('User', userSchema);
 
-async function createUser() {
-const testUser = UserModel({
-  username: 'mostafa',
-});
-
-const result = await testUser.save();
-console.log(result);
+async function createUser(req, res) {
+  const user = {
+    username: req.body.username,
+    nickname: req.body.nickname
+  };
+  const result = await UserModel(user).save();
+  if(result != null) {
+    res.json(user);
+  } else {
+    res.send(400);
+  }
 }
 
-createUser();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/users', createUser);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);

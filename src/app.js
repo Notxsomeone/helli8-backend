9c618aa076cf19ad1274f0672d444b3422a3ad0f
Mani <imnotxsomeone@gmail.com>
2024-02-06 01:21:58 +0330

    Project changed.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 
 import express from 'express';
-import userRouter from './routes/user.js';
-import { mongoose as mg } from 'mongoose';
+import http from 'http';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.use(express.json());
 
-mg.connect(process.env.DBIP + '/private')
-    .then(() => console.log('Connected to mongodb: private database'))
-    .catch(err => console.error('Could not connect to mongodb server: private database', err));
-
-app.use('/user', userRouter);
-
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT: ${PORT}`);
-})
+  console.log(`Server listening on ${PORT}`);
+});
+
+async function fetchAsync (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+var table = [];
+
+app.get('/', async (req, res) => {
+  table[0] = Object();
+  table[0].name = 'ramzinex';
+  const response = await fetchAsync('https://publicapi.ramzinex.com/exchange/api/v1.0/exchange/pairs');
+  table[0].exchangeRates = response.data.map((obj) => obj.buy / 10);
+  res.send(table);
+});

f126a2fc1ad47ab371a9287772eacd2b8c848eb7
unknown <imnotxsomeone@gmail.com>
2024-02-06 13:49:47 +0330

    Request format

--- src/app.js
+++ src/app.js
@@ -17,12 +17,99 @@ async function fetchAsync (url) {
     return data;
 }
 
-var table = [];
+var table = Object();
+const markets = [
+    "IRR",
+    "USDT",
+    "BTC",
+    "ETH",
+    "DOGE",
+    "SHIB",
+    "SOL",
+    "TRX",
+    "MATIC",
+    "TON"
+];
+const exchangeMarkets = ["ramzinex", "nobitex", "bitpin"];
 
-app.get('/', async (req, res) => {
-  table[0] = Object();
-  table[0].name = 'ramzinex';
-  const response = await fetchAsync('https://publicapi.ramzinex.com/exchange/api/v1.0/exchange/pairs');
-  table[0].exchangeRates = response.data.map((obj) => obj.buy / 10);
-  res.send(table);
+function makeTemplate() {
+  exchangeMarkets.forEach(e => {
+    table[e] = Object();
+    markets.forEach(m => {
+        table[e][m] = Object();
+    });
+  });
+}
+
+
+app.get('/', (req, res) => {
+//   table.nobitex = Object();
+//   table.BTC = Object();
+//   table[0].name = 'ramzinex';
+//   const response = await fetchAsync('https://publicapi.ramzinex.com/exchange/api/v1.0/exchange/pairs');
+//   table[0].exchangeRates = response.data.map((obj) => obj.buy);
+//   res.send(table);
+    makeTemplate();
+    res.send(table);
 });
+
+
+/*
+IRR
+USDT
+BTC
+ETH
+DOGE
+SHIB
+SOL
+TRX
+MATIC
+TON
+*/
+
+/*
+{
+    "ramzinex": {
+        "BTC": {
+            "USDT-buy": 1231321,
+            "USDT-sell": 132142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        },
+        "ETH": {
+            "USDT-buy": 1231321,
+            "USDT-sell": 132142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        }
+    },
+    "nobitex": {
+        "BTC": {
+            "USDT-buy": 1231321,
+            "USDT-sell": 132142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        },
+        "ETH": {
+            "USDT-buy": 1231321,
+            "USDT-sell": 13214142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        }
+    },
+    "bitpin": {
+        "BTC": {
+            "USDT-buy": 1321,
+            "USDT-sell": 132142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        },
+        "ETH": {
+            "USDT-buy": 1231321,
+            "USDT-sell": 132142,
+            "IRR-buy": 13231421,
+            "IRR-sell": 124141
+        }
+    }
+}
+*/

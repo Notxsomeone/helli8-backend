11132215de49fa9ff604c1b442628c9390ed7cfe
unknown <imnotxsomeone@gmail.com>
2024-03-05 14:00:59 +0330

    Database integration

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 
-const express = require('express');
+const express = require("express");
+const mg = require("mongoose")
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app   = express();
+const PORT  = process.env.PORT;
+const DB_IP = process.env.DB_IP 
 
-const markets = require("../config.json").markets;
-const brokers = require("../config.json").brokers;
-const frontendAddress= require("../config.json").frontendAddress;
+const markets         = require("../config.json").markets;
+const brokers         = require("../config.json").brokers;
+const frontendAddress = require("../config.json").frontendAddress;
 
 app.use(express.json());
 
-app.listen(PORT, () => {
+const priceSchema = new mg.Schema({
+    broker:    String,
+    name:      String,
+    irr_buy:   Number,
+    irr_sell:  Number,
+    usdt_buy:  Number,
+    usdt_sell: Number
+});
+
+const Price = mg.model("price", priceSchema);
+
+app.listen(PORT, async () => {
   makeTemplate();
   console.log(`Server listening on ${PORT}`);
+  await mg.connect(DB_IP + "/cache");
+  console.log(`Connected to database: ${DB_IP}`)
 });
 
 async function fetchAsync (url) {
@@ -32,6 +47,8 @@ function makeTemplate() {
     });
   });
 }
+
+var cached = false;
 
 app.get('/', async (req, res) => {
 
@@ -75,5 +92,28 @@ app.get('/', async (req, res) => {
 
     console.log(`Got request from ${req.ip}`);
     res.setHeader("Access-Control-Allow-Origin", frontendAddress);
+    if(!cached) {
+        cache();
+        cached = true;
+    }
     res.send(table);
 });
+
+async function cache() {
+    const brokers = Object.keys(table);
+    for (let i = 0; i < brokers.length; i++) {
+        const coins = Object.keys(table[brokers[i]]);
+        for (let j = 0; j < coins.length; j++) {
+            const values = table[brokers[i]][coins[j]];
+            const price = new Price({
+                broker: brokers[i],
+                name: coins[j],
+                irr_buy: values.irr_buy ? values.irr_buy : 0,
+                irr_sell: values.irr_sell ? values.irr_sell : 0,
+                usdt_buy: values.usdt_buy ? values.usdt_buy : 0,
+                usdt_sell: values.usdt_sell ? values.usdt_sell : 0
+            });
+            await price.save();
+        }
+    }
+}
